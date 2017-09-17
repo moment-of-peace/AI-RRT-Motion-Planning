72c167d62b74df69fc76e6d70ce73e394127780b
moment-of-peace <yi.liu1183@gmail.com>
2017-09-17 16:06:43 +1000

    find nearest, find next

--- src/rrt/Config.java
+++ src/rrt/Config.java
@@ -5,9 +5,14 @@ package rrt;
 public class Config {
     protected double[] coords;   // the coordinates of this configuration in c space
     protected Config predecessor;   // used to trace back to retrieve path
-    protected int dimension;
     
     public Config(double[] coords){
     	this.coords=coords;
+    	this.predecessor = null;
+    }
+    
+    public Config(double[] coords, Config predecessor) {
+        this.coords = coords;
+        this.predecessor = predecessor;
     }
 }
--- src/rrt/Main.java
+++ src/rrt/Main.java
@@ -8,9 +8,10 @@ import java.util.Random;
 import problem.ProblemSpec;
 
 public class Main {
-
+    final double broomLength = 0.05;
+    final double step = 0.001;
+    
 	public static void main(String[] args) throws IOException {
-		final double broomLength=0.05;
 		// load problem from a file
 	    String fileName = args[0];
 	    ProblemSpec problem = new ProblemSpec();
@@ -51,15 +52,47 @@ public class Main {
 		
 	}
 
-	public static Config findNearest(HashSet<Config> allConfig, Config target) {
+	/**
+	 * retrieve a configuration which is nearest to the sampled configuration
+	 * @param allConfig: all found configuration
+	 * @param target: the sampled configuration
+	 * @return
+	 */
+	public static Config findNearest(HashSet<Config> allConfig, Config sample) {
 	    Config result = null;
 	    double dist = Double.POSITIVE_INFINITY;
 	    double newDist;
 	    
 	    for (Config c: allConfig) {
-	        newDist = getDist(c.coord)
+	        newDist = getDist(c.coords, sample.coords);
+	        if (newDist < dist) {
+	            dist = newDist;
+	            result = c;
+	        }
 	    }
-	    return null;
+	    return result;
 	}
+
+	/**
+	 * compute the distance between two arrays
+	 * @require the size of two input arrays should be the same
+	 */
+    private static double getDist(double[] array1, double[] array2) {
+        double sum = 0;
+        for (int i = 0; i < array1.length; i++) {
+            sum += (array1[i] - array2[i]) * (array1[i] - array2[i]);
+        }
+        return sum;
+    }
+    
+    /**
+     * 
+     * @param sample: the sampled configuration
+     * @param near: nearest configuration to the sample
+     * @return: expanded configuration towards the sample from nearest
+     */
+    public static Config findNext(Config sample, Config near) {
+        return null;
+    }
 }
 
