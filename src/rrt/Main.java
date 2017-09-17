e311a65d4a8c06f2a864e6d86a79e84b933c6e77
Chrosea <475687401@qq.com>
2017-09-17 14:20:28 +1000

    Upadate random

---
+++ src/rrt/Main.java
@@ -0,0 +1,34 @@
+package rrt;
+
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int dimensions = Integer.parseInt(args[0]);
+		double[] pt = get_random_point(dimensions);
+		for (double p:pt){
+			System.out.println(p);
+		}
+	}
+	
+	public static double[] get_random_point(int dimensions)
+	{
+	    double[] pt = new double[dimensions];
+	    Random randP = new Random();
+	    //start position
+	    pt[0]= randP.nextDouble();
+	    pt[1]= randP.nextDouble();
+	    //angle
+	    for(int i = 2; i < dimensions; i++) {
+	        
+	        
+	    }
+
+	    return pt;
+	}
+
+
+}
+
